69bac9caa16312678ecbe88e3183980af6ee76f9
Wilson <wilsonandrescriollo@gmail.com>
2026-02-02 12:08:50 -0500

    Otras rutas y header completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
-import Home from "./pages/Home";
 import { ThemeProvider } from "./context/ThemeContext";
+import Home from "./pages/Home";
+import AboutMe from "./pages/AboutMe";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -9,7 +11,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/Portfolio" element={<MainLayout />}>
+            <Route path="/aboutMe" element={<AboutMe />} />
             <Route index element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,28 +1,38 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import { FiSun, FiMoon } from "react-icons/fi";
 import { useTheme } from "../context/ThemeContext";
 
 export default function MainLayout() {
   const { darkMode, setDarkMode } = useTheme();
 
+  const linkStyle = ({ isActive }) =>
+    `h-full flex items-center px-5 transition-all duration-150 whitespace-nowrap
+    ${isActive ? "border-b-2" : "hover:scale-105"}`;
+
   return (
-    <div className="bg-base flex flex-col justify-center items-center w-full gap-10">
-      <header className="relative bg-card flex items-center h-15 w-full px-6 font-semibold">
-        <nav className="absolute left-1/2 -translate-x-1/2 flex gap-5">
-          <Link>Sobre mi</Link>
-          <Link>Mis proyectos</Link>
-          <Link>Contacto</Link>
+    <div className="bg-base flex flex-col items-center w-full">
+      <header className="fixed top-0 z-10 bg-card flex items-center h-15 w-full font-semibold px-2">
+        <nav className="absolute md:left-1/2 md:-translate-x-1/2 flex h-full">
+          <NavLink to="/Portfolio/aboutMe" className={linkStyle}>
+            Sobre mi
+          </NavLink>
+          <NavLink to="/Portfolio/" className={linkStyle}>
+            Mis proyectos
+          </NavLink>
+          <NavLink to="/Portfolio/contact" className={linkStyle}>
+            Contacto
+          </NavLink>
         </nav>
 
         <button
-          className="ml-auto text-xl px-3 py-2 rounded-xl bg-base hover:bg-card"
+          className="ml-auto text-xl px-4 py-3 rounded-xl bg-base bg-btn"
           onClick={() => setDarkMode((p) => !p)}
         >
           {darkMode ? <FiSun /> : <FiMoon />}
         </button>
       </header>
 
-      <div className="w-9/10 max-w-275">
+      <div className="w-9/10 max-w-275 pt-25 pb-10">
         <Outlet />
       </div>
     </div>
